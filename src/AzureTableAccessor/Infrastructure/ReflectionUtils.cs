ef5b062d722c61880ac987ac70a1d69f99b85fc7
Maksym Tsapenko <maksym@Maksyms-MacBook-Pro.local>
2023-03-01 16:34:15 +0200

    format files

--- src/AzureTableAccessor/Infrastructure/ReflectionUtils.cs
+++ src/AzureTableAccessor/Infrastructure/ReflectionUtils.cs
@@ -14,18 +14,18 @@ namespace AzureTableAccessor.Infrastructure
             => type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
                     .FirstOrDefault(m => m.GetParameters().Length == paramCount && m.IsGenericMethod && m.Name == name);
 
-        
-		internal static void ProcessGenericInterfaceImpls(Type serviceType, Type interfaceType, Action<Type, Type, string> action)
-		{
-			if (serviceType.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == interfaceType))
-			{
-				var types = serviceType.GetInterfaces().Where(t => t.IsGenericType && t.GetGenericTypeDefinition() == interfaceType);
+
+        internal static void ProcessGenericInterfaceImpls(Type serviceType, Type interfaceType, Action<Type, Type, string> action)
+        {
+            if (serviceType.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == interfaceType))
+            {
+                var types = serviceType.GetInterfaces().Where(t => t.IsGenericType && t.GetGenericTypeDefinition() == interfaceType);
 
                 foreach (var type in types)
                 {
                     action(type, serviceType, null);
                 }
-			}
-		}
+            }
+        }
     }
 }
